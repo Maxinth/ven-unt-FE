a0c8249e9aac76366a1dfb9b34cc9fa1cbf6ef90
maxinth <tunde.j.adeleye@gmail.com>
2023-08-10 06:21:01 +0100

    ..

--- src/components/BillingHistory/index.jsx
+++ src/components/BillingHistory/index.jsx
@@ -1,5 +1,17 @@
+import Download from "../../assets/download-cloud.svg";
+
 const BillingHistory = () => {
-  return <div>BillingHistory</div>;
+  return (
+    <div className="p-4 flex items-start flex-col md:flex-row md:items-center md:justify-between mb-6">
+      <span className="text-lg text-[#101828] font-medium mb-4">
+        Billing history
+      </span>
+      <div className="flex text-sm items-center text-[#344054] border border-[#D0D5DD] rounded-lg px-4 py-[10px] font-medium">
+        <img src={Download} alt="cloud download" className="mr-2" />
+        Download all
+      </div>
+    </div>
+  );
 };
 
 export default BillingHistory;
