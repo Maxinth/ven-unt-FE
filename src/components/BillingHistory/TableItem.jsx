7d6831d1ec786b73cdf6a173610b64af30dc5747
maxinth <tunde.j.adeleye@gmail.com>
2023-08-10 09:10:43 +0100

    TableItem data

--- src/components/BillingHistory/TableItem.jsx
+++ src/components/BillingHistory/TableItem.jsx
@@ -1,11 +1,15 @@
 import CheckIcon from "../../assets/Checkbox.svg";
+import DownloadCloud from "../../assets/download-cloud.svg";
+import PeopleImg from "../../assets/5-people-table-img.svg";
 
 const TableItem = () => {
   return (
-    <div className="px-4 w-full bg-[#FFF] text-sm leading-[20px] font-medium text-[#667085] flex md:justify-between items-center ">
+    <div className="px-4 border border-t-[#D0D5DD] w-full bg-[#FFF] text-sm leading-[20px] font-medium text-[#667085] flex md:justify-between items-center ">
       <div className="flex items-center cursor-pointer w-[229px] lg:min-w-[495px]">
         <img src={CheckIcon} alt="check" className="cursor-pointer" />
-        <span className="ml-3 mr-1 min-w-[150px]">Basic Plan – Dec 2022</span>
+        <span className="ml-3 mr-1 min-w-[150px] px-6 py-4">
+          Basic Plan – Dec 2022
+        </span>
       </div>
       <div className="w-full flex item-center ml-4 !bg-[#FFF]">
         <span className="px-6 py-3 min-w-[160px] flex items-center justify-center">
@@ -18,7 +22,14 @@ const TableItem = () => {
           Paid
         </span>
         <span className="px-6 py-3 break-words min-w-[200px]  flex items-center justify-end bg-[#FFF]">
-          Users on plan akdkdk
+          <img src={PeopleImg} alt="people" />
+        </span>
+        <span className="px-6 py-3    flex items-center justify-end bg-[#FFF]">
+          <img
+            src={DownloadCloud}
+            alt="download cloud"
+            className="cursor-pointer"
+          />
         </span>
       </div>
     </div>
